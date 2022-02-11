efba5616521a8b8802d7518fb6bcb869925386f0
jollypyun <lucas.stave@gmail.com>
2022-02-11 19:51:00 +0900

    Setting for Spring Data JPA

---
+++ src/main/java/com/example/musicat_audio/repository/PlaylistRepository2.java
@@ -0,0 +1,8 @@
+//package com.example.musicat_audio.repository;
+//
+//import com.example.musicat_audio.domain.Playlist;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface PlaylistRepository2 extends JpaRepository<Playlist, String> {
+//    Playlist savePlaylist(Playlist playlist);
+//}
