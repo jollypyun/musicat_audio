c0182e9cfcd2b1c98d61920999f703aed57fecff
Geunhyung Pyun <58629366+jollypyun@users.noreply.github.com>
2022-02-21 14:08:36 +0900

    Delete PlaylistRepository2.java

--- src/main/java/com/example/musicat_audio/repository/PlaylistRepository2.java
+++
@@ -1,8 +0,0 @@
-//package com.example.musicat_audio.repository;
-//
-//import com.example.musicat_audio.domain.Playlist;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface PlaylistRepository2 extends JpaRepository<Playlist, String> {
-//    Playlist savePlaylist(Playlist playlist);
-//}
