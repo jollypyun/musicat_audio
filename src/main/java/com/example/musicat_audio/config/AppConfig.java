1006fcd4c4b8b5313147d75813ccfd2a2ffdbd69
fidget278 <mills_club@naver.com>
2022-06-27 21:17:25 +0900

    cors 설정 변경

--- src/main/java/com/example/musicat_audio/config/AppConfig.java
+++ src/main/java/com/example/musicat_audio/config/AppConfig.java
@@ -12,7 +12,7 @@ public class AppConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:8080", "http://musicat.life")
                 .allowedMethods("*")
                 .allowedHeaders("*");
     }
